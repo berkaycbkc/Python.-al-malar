706e935ad50824640b06fe9e789ff2ed5adf2257
berkaycbkc <144543797+berkaycbkc@users.noreply.github.com>
2023-09-16 16:18:57 +0300

    HHJ

---
+++ pyt0.py
@@ -0,0 +1,11 @@
+import random
+print("                                   YEMEK LİSTESİ")
+corba=['mercimek','tarhana','ezogelin']
+yemek=['pilav','lahmacun','tavuk pılav']
+ıcecek=['kola','gazos','soda']
+a= random.choice(corba)
+b=(random.choice(yemek))
+c=(random.choice(ıcecek))
+print("size corba olarak",a)
+print("size yemek olarak",b)
+print("yanınada içecek olarak",c)
