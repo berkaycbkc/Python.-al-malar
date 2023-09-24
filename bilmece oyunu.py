ee203776157642d7c308c58b5387213914a4a193
berkaycbkc <144543797+berkaycbkc@users.noreply.github.com>
2023-09-24 18:59:35 +0300

    Add files via upload

---
+++ bilmece oyunu.py
@@ -0,0 +1,29 @@
+print("-------BİLMECE OYUNU-------")
+b=input("başlamak için tıklayın...")
+c='diş'
+a=input("32 beyaz atlı bir kızıl tepede önce çarpışır sonra susar?:")
+
+if a == c:
+   print("--bildin...bu şanstı--\n devam edelim")
+
+else:
+   print("bilemedin")
+   print("bir kere daha şansını dene bence")
+print("===============================================================")
+c1='rüzgar'
+a1=input("sesiz bağırır kanatsız çırpışır ağızsız mırıldanır?:")
+
+if a1 == c1:
+   print("--bildin--\n bu sefer zor olucak")
+
+else:
+   print("yine bilemedin.Sen biraz kötüsün bu konuda")
+print("================================================================")
+c2='yumurta'
+a2=input("bir kutu menteşesiz anahtarsız ve kapaksız yinede saklı içinde altından bir hazine?: ")
+
+if a2==c2:
+   print("bunu bilemezsin sanmıştım ama yanılmışım")
+
+else:
+   print("bilemedin salak")
