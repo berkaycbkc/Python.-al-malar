cd99177a936c49ae4ab59642c6e642777f468c5e
Berkay Çubukçu <144543797+berkaycbkc@users.noreply.github.com>
2023-10-08 19:03:53 +0300

    Add files via upload

---
+++ yetenek.py
@@ -0,0 +1,13 @@
+para=int(input("ne kadar xp in var?"))
+
+if para >= 100:
+   print("5 tane yetenek alabilirsin")
+
+if para >= 85:
+   print("4 tane yetenek alabilirsin veya")
+
+if para >= 70:
+   print("3 tane yetenek alabilirsiniz")
+
+else:
+    print("yetenek alamazsın GİT VE DAHA FAZLA DÜŞMAN ÖLDÜR!")
